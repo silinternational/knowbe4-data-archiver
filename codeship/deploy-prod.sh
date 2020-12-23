cc265a5b8de0edadbced7c76b2e312880ca40d43
BaggerOne <baggerone@gmail.com>
2020-12-23 09:45:26 -0500

    Add CI/CD files

---
+++ codeship/deploy-prod.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Exit script with error if any step fails.
+set -e
+
+# Build binaries
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/build.sh
+
+# Export env vars
+export AWS_REGION="${AWS_REGION}"
+export API_BASE_URL="${API_BASE_URL}"
+export API_AUTH_TOKEN="${API_AUTH_TOKEN}"
+
+serverless deploy -v --stage prod
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/silinternational/knowbe4-data-archiver
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/aws/aws-lambda-go v1.21.0
+	github.com/stretchr/testify v1.6.1
+)
