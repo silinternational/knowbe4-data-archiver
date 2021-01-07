c8cfb03e48296781235b743a5b72167331397b71
BaggerOne <baggerone@gmail.com>
2021-01-07 08:47:03 -0500

    Get all pages of results, not just one

--- archiver/main.go
+++ archiver/main.go
@@ -16,25 +16,53 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
+const countPerPage = 500
 const securityTestURLPath = "v1/phishing/security_tests"
 const s3DefaultFilename = "knowbe4_data"
 
-const EnvAPIBaseURL = "API_BASE_URL"
-const EnvAPIAuthToken = "API_AUTH_TOKEN"
-const EnvAWSS3Filename = "AWS_S3_FILENAME"
+const (
+	EnvAPIBaseURL = "API_BASE_URL"
+	EnvAPIAuthToken = "API_AUTH_TOKEN"
+	EnvAWSS3Filename = "AWS_S3_FILENAME"
+)
 
-func getRequiredString(envKey string, configEntry *string) error {
-	if *configEntry != "" {
-		return nil
-	}
 
-	value := os.Getenv(envKey)
-	if value == "" {
-		return fmt.Errorf("required value missing for environment variable %s", envKey)
-	}
-	*configEntry = value
-
-	return nil
+type KnowBe4SecurityTest struct {
+	CampaignID int    `json:"campaign_id"`
+	PstID      int    `json:"pst_id"`
+	Status     string `json:"status"`
+	Name       string `json:"name"`
+	Groups     []struct {
+		GroupID int    `json:"group_id"`
+		Name    string `json:"name"`
+	} `json:"groups"`
+	PhishPronePercentage float64   `json:"phish_prone_percentage"`
+	StartedAt            time.Time `json:"started_at"`
+	Duration             int       `json:"duration"`
+	Categories           []struct {
+		CategoryID int    `json:"category_id"`
+		Name       string `json:"name"`
+	} `json:"categories"`
+	Template struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"template"`
+	LandingPage struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"landing-page"`
+	ScheduledCount        int `json:"scheduled_count"`
+	DeliveredCount        int `json:"delivered_count"`
+	OpenedCount           int `json:"opened_count"`
+	ClickedCount          int `json:"clicked_count"`
+	RepliedCount          int `json:"replied_count"`
+	AttachmentOpenCount   int `json:"attachment_open_count"`
+	MacroEnabledCount     int `json:"macro_enabled_count"`
+	DataEnteredCount      int `json:"data_entered_count"`
+	VulnerablePluginCount int `json:"vulnerable_plugin_count"`
+	ExploitedCount        int `json:"exploited_count"`
+	ReportedCount         int `json:"reported_count"`
+	BouncedCount          int `json:"bounced_count"`
 }
 
 type LambdaConfig struct {
@@ -64,45 +92,23 @@ func (c *LambdaConfig) init() error {
 	return nil
 }
 
-type KnowBe4SecurityTest struct {
-	CampaignID int    `json:"campaign_id"`
-	PstID      int    `json:"pst_id"`
-	Status     string `json:"status"`
-	Name       string `json:"name"`
-	Groups     []struct {
-		GroupID int    `json:"group_id"`
-		Name    string `json:"name"`
-	} `json:"groups"`
-		PhishPronePercentage float64   `json:"phish_prone_percentage"`
-		StartedAt            time.Time `json:"started_at"`
-		Duration             int       `json:"duration"`
-		Categories           []struct {
-		CategoryID int    `json:"category_id"`
-		Name       string `json:"name"`
-	} `json:"categories"`
-	Template struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"template"`
-	LandingPage struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	} `json:"landing-page"`
-	ScheduledCount        int `json:"scheduled_count"`
-	DeliveredCount        int `json:"delivered_count"`
-	OpenedCount           int `json:"opened_count"`
-	ClickedCount          int `json:"clicked_count"`
-	RepliedCount          int `json:"replied_count"`
-	AttachmentOpenCount   int `json:"attachment_open_count"`
-	MacroEnabledCount     int `json:"macro_enabled_count"`
-	DataEnteredCount      int `json:"data_entered_count"`
-	VulnerablePluginCount int `json:"vulnerable_plugin_count"`
-	ExploitedCount        int `json:"exploited_count"`
-	ReportedCount         int `json:"reported_count"`
-	BouncedCount          int `json:"bounced_count"`
+
+func getRequiredString(envKey string, configEntry *string) error {
+	if *configEntry != "" {
+		return nil
+	}
+
+	value := os.Getenv(envKey)
+	if value == "" {
+		return fmt.Errorf("required value missing for environment variable %s", envKey)
+	}
+	*configEntry = value
+
+	return nil
 }
 
-func callAPI(urlPath string, config LambdaConfig) (*http.Response, error) {
+
+func callAPI(urlPath string, config LambdaConfig, queryParams map[string]string) (*http.Response, error) {
 	var err error
 	var req *http.Request
 
@@ -114,7 +120,14 @@ func callAPI(urlPath string, config LambdaConfig) (*http.Response, error) {
 	}
 
 	req.Header.Set("Authorization", "Bearer " + config.APIAuthToken)
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	// Add query parameters
+	q := req.URL.Query()
+	for key, val := range queryParams {
+		q.Add(key, val)
+	}
+	req.URL.RawQuery = q.Encode()
 
 	client := &http.Client{}
 
@@ -131,9 +144,14 @@ func callAPI(urlPath string, config LambdaConfig) (*http.Response, error) {
 	return resp, nil
 }
 
-func getReport(config LambdaConfig) ([]byte, []KnowBe4SecurityTest, error) {
+func getSecurityTestsPage(pageNum int, config LambdaConfig) ([]byte, []KnowBe4SecurityTest, error) {
+	queryParams := map[string]string{
+		"per_page": strconv.Itoa(countPerPage),
+		"page": strconv.Itoa(pageNum),
+	}
+
 	// Make http call
-	resp, err := callAPI(securityTestURLPath, config)
+	resp, err := callAPI(securityTestURLPath, config, queryParams)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -141,14 +159,35 @@ func getReport(config LambdaConfig) ([]byte, []KnowBe4SecurityTest, error) {
 	defer resp.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
 
-	var allTests []KnowBe4SecurityTest
+	var pageTests []KnowBe4SecurityTest
 
-	s, _ := strconv.Unquote(string(bodyBytes))
-	if err := json.Unmarshal([]byte(s), &allTests); err != nil {
+	if err := json.Unmarshal(bodyBytes, &pageTests); err != nil {
 		return nil, nil, fmt.Errorf("error decoding response json for security tests: %s", err)
 	}
 
-	return bodyBytes, allTests, nil
+	return bodyBytes, pageTests, nil
+}
+
+func getAllSecurityTests(config LambdaConfig) ([]byte, []KnowBe4SecurityTest, error) {
+	var allData []byte
+	var allTests []KnowBe4SecurityTest
+
+	for i := 1;; i++ {
+		data, nextTests, err := getSecurityTestsPage(i, config)
+		if err != nil {
+			err = fmt.Errorf("error fetching page %v ... %s", i, err)
+			return nil, nil, err
+		}
+
+		allData = append(allData, data...)
+		allTests = append(allTests, nextTests...)
+
+		if len(nextTests) < countPerPage {
+			break
+		}
+	}
+
+	return allData, allTests, nil
 }
 
 func saveToS3(data []byte, config LambdaConfig) error {
@@ -172,7 +211,7 @@ func handler(config LambdaConfig) error {
 		return err
 	}
 
-	data, _, err := getReport(config)
+	data, _, err := getAllSecurityTests(config)
 	if err != nil {
 		return err
 	}
