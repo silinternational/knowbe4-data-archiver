dadedd5314c8eccdb2c8e526d38c844bf24c310b
BaggerOne <baggerone@gmail.com>
2021-01-07 09:39:47 -0500

    Add env vars

--- codeship/deploy-prod.sh
+++ codeship/deploy-prod.sh
@@ -11,5 +11,7 @@ $DIR/build.sh
 export AWS_REGION="${AWS_REGION}"
 export API_BASE_URL="${API_BASE_URL}"
 export API_AUTH_TOKEN="${API_AUTH_TOKEN}"
+export AWS_S3_FILENAME="${AWS_S3_FILENAME}"
+export AWS_S3_BUCKET="${AWS_S3_BUCKET}"
 
 serverless deploy -v --stage prod
